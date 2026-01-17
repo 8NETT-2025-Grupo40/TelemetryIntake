7d0258e88b8bb1545036926c6582c124ee5ecfec
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2026-01-16 21:29:17 -0300

    Create classes to configure application.

---
+++ TelemetryIntake.API/DependencyInjection/DocumentationServices.cs
@@ -0,0 +1,11 @@
+﻿namespace TelemetryIntake.API.DependencyInjection;
+
+public static class DocumentationServices
+{
+	public static void AddDocumentationServices(this IHostApplicationBuilder applicationBuilder)
+	{
+		applicationBuilder.Services.AddOpenApi();
+		applicationBuilder.Services.AddEndpointsApiExplorer();
+		applicationBuilder.Services.AddSwaggerGen();
+	}
+}
---
+++ TelemetryIntake.API/Setup/DocumentationSetup.cs
@@ -0,0 +1,14 @@
+﻿namespace TelemetryIntake.API.Setup;
+
+public static class DocumentationSetup
+{
+	public static void ConfigureDocumentation(this WebApplication webApplication)
+	{
+		if (webApplication.Environment.IsDevelopment())
+		{
+			webApplication.MapOpenApi();
+			webApplication.UseSwagger();
+			webApplication.UseSwaggerUI();
+		}
+	}
+}
