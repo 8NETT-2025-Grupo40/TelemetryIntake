b9c25fe40d07bdf380b7cc0e07c14e9aef8cdc6e
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2026-01-24 13:06:23 -0300

    Create real health check for AWS SQS.

--- TelemetryIntake.API/HealthChecks/QueueHealthCheck.cs
+++ TelemetryIntake.API/HealthChecks/QueueHealthCheck.cs
@@ -1,12 +1,61 @@
-﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using TelemetryIntake.Application.Results;
+using TelemetryIntake.Infrastructure.Messaging;
 
 namespace TelemetryIntake.API.HealthChecks;
 
 public class QueueHealthCheck : IHealthCheck
 {
-	public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+	private readonly IOptions<SqsOptions> _sqsOptions;
+	private readonly IAmazonSQS _amazonSQSClient;
+	private readonly ILogger<QueueHealthCheck> _logger;
+
+	public QueueHealthCheck(
+		IOptions<SqsOptions> sqsOptions,
+		IAmazonSQS amazonSQSClient,
+		ILogger<QueueHealthCheck> logger)
 	{
-		// TODO: Modificar isso para que realmente seja uma validação de saúde por meio da fila SQS.
-		return Task.FromResult(HealthCheckResult.Healthy());
+		_sqsOptions = sqsOptions;
+		_amazonSQSClient = amazonSQSClient;
+		_logger = logger;
+	}
+
+	public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+	{
+		var healthyResult = await ValidateQueue();
+
+		if (healthyResult.IsSuccessful)
+		{
+			return HealthCheckResult.Healthy("Queue is ready to receive messages.");
+		}
+
+		return HealthCheckResult.Unhealthy(healthyResult.Message);
+	}
+
+	private async ValueTask<Result> ValidateQueue()
+	{
+		try
+		{
+			var request = new GetQueueAttributesRequest
+			{
+				QueueUrl = _sqsOptions.Value.QueueUrl,
+				AttributeNames = [QueueAttributeName.QueueArn]
+			};
+
+			 _ = await _amazonSQSClient.GetQueueAttributesAsync(request);
+			return Result.Success();
+		}
+		catch (Exception e)
+		{
+			const string message = "There was an error on the Queue Health Check";
+			_logger.LogError(e, message);
+
+			var errorMessage = $"{message}\n\n{e}";
+
+			return Result.Error(errorMessage);
+		}
 	}
 }
---
+++ TelemetryIntake.Application/Results/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace TelemetryIntake.Application.Results;
+
+public class Result
+{
+	private Result(string? message, bool isSuccessful)
+	{
+		Message = message;
+		IsSuccessful = isSuccessful;
+	}
+
+	public string? Message { get; set; }
+	public bool IsSuccessful { get; set; }
+
+	public static Result Success() =>
+		new("Success", true);
+
+	public static Result Error(string message) => new(message, false);
+}
